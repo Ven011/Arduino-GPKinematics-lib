f99c09ffa1e151ee9a4eda542e3d0438b236d024
Tevin <deskcomp10@outlook.com>
2022-06-14 02:35:49 -0400

    working coordinate grid class. Fun animation included

--- scripts/matrix_ops.py
+++ scripts/matrix_ops.py
@@ -9,9 +9,9 @@ import pygame.gfxdraw as pyg_gfx
 from math import *
 from math import pi
 
-class MOPS():
-    def __init__(self):
-        self.angle = 0
+class Coordinate_grid():
+    def __init__(self, screen, pixel_axis_length = 200, perceived_axis_length = 400, grid_rows = 10, grid_columns = 10):
+        self.scrn = screen
     
         # used to project 3D points (x, y, z) to 2D points (x, y)
         self.projection_matrix = np.matrix([
@@ -20,39 +20,157 @@ class MOPS():
             [0, 0, 0]
         ])
         
-        self.prev_theta =   1
-        self.prev_alpha =   1
-        self.prev_sigma =   1
-        self.theta =        0   # x angle of the center point
-        self.alpha =        0   # y angle of the center point
-        self.sigma =        0   # z axis angle of the center point
+        self.proj_center_p = []
+        self.grid = []
+    
+        self.axis_len = perceived_axis_length    # perceived length of the axes in centimeters
+        self.grid_rows = grid_rows
+        self.grid_cols = grid_columns 
+
+        self.theta =    0   # x angle of the center point
+        self.alpha =    0   # y angle of the center point
+        self.sigma =    0   # z axis angle of the center point
         
         # Displacement vectors
-        self.V_Z =      np.zeros((3, 1))   
-        self.V_Y =      np.zeros((3, 1))   
-        self.V_X =      np.zeros((3, 1))   
+        self.V_Z =      np.zeros((3, 1))
+        self.V_Y =      np.zeros((3, 1))
+        self.V_X =      np.zeros((3, 1))
         
-        self.LZ =       200  # length from z axis endpoint to center point
-        self.LY =       200
-        self.LX =       200
+        self.LZ =       pixel_axis_length  # length from z axis endpoint to center point
+        self.LY =       pixel_axis_length
+        self.LX =       pixel_axis_length
         
         # used to draw axis lines
-        self.projected_points = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-        self.ws_points = [[0, 0], [0, 0], [0, 0], [0, 0]]
+        self.axis_points = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] # [center, +z, +y, +x, center, -z, -y, -x]
+        self.ws_edge_points = [[0, 0], [0, 0], [0, 0], [0, 0]]                              # [(xy), (x,-y), (-x, -y), (-x, y)]
         
-    def refresh_workspace(self):
-        """
-        Decides whether to recalculate the workspace axes and plane points
-        """
-        if self.prev_theta == self.theta and self.prev_sigma == self.sigma and self.prev_alpha == self.alpha:
-            return False
+    def run(self, center, events):
+        self.proj_center_p = np.matrix(center).reshape((3, 1))
+        
+        # create illusion of 3D
+        self.proj_center_p[1, 0] = self.proj_center_p[1, 0] - (self.proj_center_p[2, 0] * self.theta * 1.27)
+        
+        # display the coordinate grid
+        self.show_grid(show_grid_points=False)
+        
+        for event in events:
+            if event.type == pyg.KEYDOWN:
+                if event.key == pyg.K_UP:
+                    self.theta += 0.01
+                if event.key == pyg.K_DOWN:
+                    self.theta -= 0.01
+                if event.key == pyg.K_LEFT:
+                    self.alpha += 0.01
+                if event.key == pyg.K_RIGHT:
+                    self.alpha -= 0.01
+                if event.key == pyg.K_COMMA:
+                    self.sigma -= 0.01
+                if event.key == pyg.K_PERIOD:
+                    self.sigma += 0.01
+        
+    def get_point_at_XYcoord(self, coord):
+        # Uses the concept of ratios. If you think the distance from |---| is 10cm but i represent that distance as |-------| which is 100 cm
+        # then point (10, 0) to you is point (100, 0) to me. Using this ratio I can know what you mean by 5cm -> 50cm
+        
+        # get the center point of the coordinate grid
+        x = self.grid[(self.grid_rows//2), (self.grid_cols//2), 0]
+        y = self.grid[(self.grid_rows//2), (self.grid_cols//2), 1]
+        
+        # get the point immediately diagnal to the center point in the first quadrant
+        x_n = self.grid[(self.grid_rows//2) - 1, (self.grid_cols//2) - 1, 0]
+        y_n = self.grid[(self.grid_rows//2) - 1, (self.grid_cols//2) - 1, 1]
+        
+        # find the y distance from the center y to the diagnal point y
+        y_dis = y_n - y
+        # same for x
+        x_dis = x_n - x
+        
+        # determine the perceived grid box length
+        per_gb_len = self.axis_len / self.grid_cols
+        
+        # calculate the point
+        point = [x + ((x_dis * coord[0]) / per_gb_len), y + ((y_dis * coord[1]) / per_gb_len)]
+        
+        return point
+        
+    def connect_axis_points(self, i, j, color):
+        pyg.draw.line(self.scrn, color, (self.axis_points[i][0], self.axis_points[i][1]), (self.axis_points[j][0], self.axis_points[j][1])) 
+        
+    def update_grid_points(self, draw=False):
+        x_grid_boxes, y_grid_boxes = self.grid_rows, self.grid_cols
+        
+        # get variables
+        xy_p = [self.ws_edge_points[0][0], self.ws_edge_points[0][1]]     # (x, y) point
+        nxy_p = [self.ws_edge_points[3][0], self.ws_edge_points[3][1]]    # (-x, y)
+        xny_p = [self.ws_edge_points[1][0], self.ws_edge_points[1][1]]    # (x, -y)
+        
+        xy_to_nxy_dist_x = abs(xy_p[0] - nxy_p[0])  # distances
+        xy_to_nxy_dist_y = abs(xy_p[1] - nxy_p[1])
+        
+        # calculate the grid gap of each axis
+        x_grid_gap = (xy_to_nxy_dist_x / x_grid_boxes)
+        y_grid_gap = (xy_to_nxy_dist_y / y_grid_boxes)
+        
+        xy_to_nxy_slope_x = xy_p[0] - nxy_p[0]
+        xy_to_nxy_slope_y = xy_p[1] - nxy_p[1]
+        xy_to_xny_slope_x = xy_p[0] - xny_p[0]
+        xy_to_xny_slope_y = xy_p[1] - xny_p[1]
+        
+        # create a list of x coordinates from xy_p to nxy_p
+        x_coords = []
+        y_coords = []
+            # do not try to create a ranging list with gaps if the grid gap is 0.
+        if x_grid_gap: 
+                # create the appropriate x coordinates list depending on the coordinate point that is on the left relative to the other on the screen
+            if xy_to_nxy_slope_x > 0:   # xy_p is to the right relative to nxy_p
+                x_coords = list(np.arange(xy_p[0], nxy_p[0] - 1, -x_grid_gap))
+            else:                       # they are level or xy_p is to the left
+                x_coords = list(np.arange(xy_p[0], nxy_p[0] + 1, x_grid_gap))
         else:
-            return True
+            x_coords = [xy_p[0]]
+                
+            # do not try to create a ranging list with gaps if the grid gap is 0.
+        if y_grid_gap: 
+                # create the appropriate x coordinates list depending on the coordinate point that is on the above relative to the other on the screen
+            if xy_to_nxy_slope_y > 0:   # xy_p is below relative to nxy_p
+                y_coords = list(np.arange(xy_p[1], nxy_p[1] - 1, -y_grid_gap))
+            else:                       # they are level or xy_p is above
+                y_coords = list(np.arange(xy_p[1], nxy_p[1] + 1, y_grid_gap))
+        else:
+            y_coords = [xy_p[1]]
+            
+            # turn each element of the lists to an int 
+        x_coords = [int(x) for x in x_coords]
+        y_coords = [int(y) for y in y_coords]
         
-    def connect_points(self, i, j, points, scrn, color):
-        pyg.draw.line(scrn, color, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+        # create and fill workspace grid matrix
+        self.grid = np.zeros((y_grid_boxes + 1, x_grid_boxes + 1, 2))
         
-    def project_workspace(self, points, width, height, scrn):  
+            # fill the x and y coordinates lists to match the shape of the ws_grid if they are not filled to that shape
+        if len(x_coords) < x_grid_boxes + 1:
+            for _ in range(x_grid_boxes + 1 - len(x_coords)):
+                x_coords.append(x_coords[0])
+                
+        if len(y_coords) < y_grid_boxes + 1:
+            for _ in range(y_grid_boxes + 1 - len(y_coords)):
+                y_coords.append(y_coords[0])
+        
+        for row in range(self.grid.shape[0]):
+            for col in range(self.grid.shape[1]):
+                self.grid[row, col, 0] = x_coords[col] - (xy_to_xny_slope_x / x_grid_boxes * row)
+                self.grid[row, col, 1] = y_coords[col] - (xy_to_xny_slope_y / y_grid_boxes * row)
+                if draw:
+                    pyg.draw.circle(self.scrn, (25, 25, 25), (self.grid[row, col]), 1)
+        
+    def show_grid(self, show_grid_points=False): 
+        # create axis endpoints
+        points = []
+        
+        points.append(np.matrix([self.proj_center_p[0, 0], self.proj_center_p[1, 0], self.proj_center_p[2, 0]]))    # center point
+        points.append(self.get_z_endpoint())
+        points.append(self.get_y_endpoint())
+        points.append(self.get_x_endpoint())
+         
         i = 0
         for point in points:
             # turn 3D points into 2D points
@@ -61,53 +179,63 @@ class MOPS():
             x = int(projected3D[0][0])
             y = int(projected3D[1][0])
             
-            self.projected_points[i] = [x, y]
-            self.projected_points[i + 4] = [(width/2) - (x - (width/2)), (height/2) - (y - (height/2))]
+            self.axis_points[i] = [x, y]
+            self.axis_points[i + 4] = [(self.proj_center_p[0, 0]) - (x - (self.proj_center_p[0, 0])), (self.proj_center_p[1, 0]) - (y - (self.proj_center_p[1, 0]))]
             i += 1
             
-                # calculate workspace plane points. Used the concepts of parallel lines and distance
+        # calculate workspace plane points. Used the concepts of parallel lines and distance
         # x,y point
-        self.ws_points[0][0] = self.projected_points[2][0] - (self.projected_points[0][0] - self.projected_points[3][0])
-        self.ws_points[0][1] = self.projected_points[2][1] - (self.projected_points[0][1] - self.projected_points[3][1])
-        # -x,y point
-        self.ws_points[2][0] = self.projected_points[6][0] - (self.projected_points[0][0] - self.projected_points[7][0])
-        self.ws_points[2][1] = self.projected_points[6][1] - (self.projected_points[0][1] - self.projected_points[7][1])
-        # -x,-y
-        self.ws_points[1][0] = self.projected_points[3][0] + (self.projected_points[0][0] - self.projected_points[2][0])
-        self.ws_points[1][1] = self.projected_points[3][1] + (self.projected_points[0][1] - self.projected_points[2][1])
+        self.ws_edge_points[0][0] = self.axis_points[2][0] - (self.axis_points[0][0] - self.axis_points[3][0])    # x
+        self.ws_edge_points[0][1] = self.axis_points[2][1] - (self.axis_points[0][1] - self.axis_points[3][1])    # y
+        # -x,-y point
+        self.ws_edge_points[2][0] = self.axis_points[6][0] - (self.axis_points[0][0] - self.axis_points[7][0])
+        self.ws_edge_points[2][1] = self.axis_points[6][1] - (self.axis_points[0][1] - self.axis_points[7][1])
         # x,-y
-        self.ws_points[3][0] = self.projected_points[7][0] + (self.projected_points[0][0] - self.projected_points[6][0])
-        self.ws_points[3][1] = self.projected_points[7][1] + (self.projected_points[0][1] - self.projected_points[6][1])
-        
-        self.prev_theta = self.theta
-        self.prev_alpha = self.alpha
-        self.prev_sigma = self.sigma
+        self.ws_edge_points[1][0] = self.axis_points[3][0] + (self.axis_points[0][0] - self.axis_points[2][0])
+        self.ws_edge_points[1][1] = self.axis_points[3][1] + (self.axis_points[0][1] - self.axis_points[2][1])
+        # -x,y
+        self.ws_edge_points[3][0] = self.axis_points[7][0] + (self.axis_points[0][0] - self.axis_points[6][0])
+        self.ws_edge_points[3][1] = self.axis_points[7][1] + (self.axis_points[0][1] - self.axis_points[6][1])
            
         # draw axes lines 
-        self.connect_points(0, 1, self.projected_points, scrn, (0, 0, 255)) # draws z axis
-        self.connect_points(0, 2, self.projected_points, scrn, (0, 255, 0)) #       y
-        self.connect_points(0, 3, self.projected_points, scrn, (255, 0, 0)) #       x
-        self.connect_points(0, 5, self.projected_points, scrn, (0, 0, 255)) # draws neg z axis
-        self.connect_points(0, 6, self.projected_points, scrn, (0, 255, 0)) #           y
-        self.connect_points(0, 7, self.projected_points, scrn, (255, 0, 0)) #           x
+        self.connect_axis_points(0, 1, (0, 0, 255)) # draws z axis
+        self.connect_axis_points(0, 2, (0, 255, 0)) #       y
+        self.connect_axis_points(0, 3, (255, 0, 0)) #       x
+        self.connect_axis_points(0, 5, (0, 0, 255)) # draws neg z axis
+        self.connect_axis_points(0, 6, (0, 255, 0)) #           y
+        self.connect_axis_points(0, 7, (255, 0, 0)) #           x
+        
+        # draw origin point
+        pyg.draw.circle(self.scrn, (255, 23, 241), ([self.proj_center_p[0, 0], self.proj_center_p[1, 0]]), 4)
+        
+        # draw points to indicate the first octant of the workspace
+        pyg.draw.circle(self.scrn, (0, 0, 0), tuple(self.axis_points[1]), 3)
+        pyg.draw.circle(self.scrn, (0, 0, 0), tuple(self.axis_points[2]), 3)
+        pyg.draw.circle(self.scrn, (0, 0, 0), tuple(self.axis_points[3]), 3)
         
         # draw workspace plane
-        pyg_gfx.filled_polygon(scrn, [(x, y) for x, y in self.ws_points], (0, 0, 0, 50))
+        pyg_gfx.filled_polygon(self.scrn, [(x, y) for x, y in self.ws_edge_points], (0, 0, 0, 50))
+        
+        # update the grid points
+        if show_grid_points:
+            self.update_grid_points(show_grid_points)
+        else:
+            self.update_grid_points()
     
-    def get_z_endpoint(self, width, height):
+    def get_z_endpoint(self):
         self.calc_DVs()
         # return the x, y, z coordinate of the Z endpoint
-        return np.matrix([self.V_Z[0, 0] + width/2, self.V_Z[1, 0] + height/2, self.V_Z[2, 0]])
+        return np.matrix([self.V_Z[0, 0] + self.proj_center_p[0, 0], self.V_Z[1, 0] + self.proj_center_p[1, 0], self.V_Z[2, 0]])
     
-    def get_y_endpoint(self, width, height):
+    def get_y_endpoint(self):
         self.calc_DVs()
         # return the x, y, z coordinate of the Y endpoint
-        return np.matrix([self.V_Y[0, 0] + width/2, self.V_Y[1, 0] + height/2, self.V_Y[2, 0]])
+        return np.matrix([self.V_Y[0, 0] + self.proj_center_p[0, 0], self.V_Y[1, 0] + self.proj_center_p[1, 0], self.V_Y[2, 0]])
     
-    def get_x_endpoint(self, width, height):
+    def get_x_endpoint(self):
         self.calc_DVs()
         # return the x, y, z coordinate of the X endpoint
-        return np.matrix([self.V_X[0, 0] + width/2, self.V_X[1, 0] + height/2, self.V_X[2, 0]])
+        return np.matrix([self.V_X[0, 0] + self.proj_center_p[0, 0], self.V_X[1, 0] + self.proj_center_p[1, 0], self.V_X[2, 0]])
 
     def calc_DVs(self):
         # calculate the displacement vector for the Z axis endpoint
@@ -202,7 +330,28 @@ class Grid():
         """
         self._x, self._y, self._width, self._height = grid_dimensions
     
+# class Robot_joint():
+#     def __init__(self, joint_pos = [0, 0, 0], rotation_angle = ["XY", [-pi/2, pi/2]], link_length = 0):
+#         # initialize joint variables
+#         self.pos = joint_pos
+#         self.rot_plane = rotation_angle[0]
+#         self.rot_angle = rotation_angle[1]
+#         self.link_len = link_length
+        
+#     def show(self, workspace_grid):
+#         # place robot joint point on the workspace at the specified joint position
+        
     
+# class Robot():
+#     def __init__(self):
+#         self.joint = []
+    
+#     def create_joint(self):
+        
+    
+#     def place(self):
+#         pass
+        
 
     
     
--- scripts/model.py
+++ scripts/model.py
@@ -9,41 +9,35 @@ import pygame_widgets as pyg_wid
 from pygame_widgets.textbox import TextBox
 import numpy as np
 from math import *
-from matrix_ops import MOPS
+from matrix_ops import Coordinate_grid
 from menu_ops import Menu
-
-mops = MOPS()
     
-def handle_model_viz(width, height, scrn, events):
-    # create the workspace axes and plane
-    points = []
-    center_point = np.matrix([width//2, height//2, 0])
-    pyg.draw.circle(scrn, (255, 23, 241), (center_point[0, 0], center_point[0, 1]), 3)
-
-    z_endpoint = mops.get_z_endpoint(width, height)
-    y_endpoint = mops.get_y_endpoint(width, height)
-    x_endpoint = mops.get_x_endpoint(width, height)
+ang = 0
     
-    points.append(center_point)
-    points.append(z_endpoint)
-    points.append(y_endpoint)
-    points.append(x_endpoint)
-    mops.project_workspace(points, width, height, scrn)
+def handle_workspace(ws, center_point ,events, ws2, ws3, ws4):
+    global ang
     
-    for event in events:
-        if event.type == pyg.KEYDOWN:
-            if event.key == pyg.K_UP:
-                mops.theta += 0.01
-            if event.key == pyg.K_DOWN:
-                mops.theta -= 0.01
-            if event.key == pyg.K_LEFT:
-                mops.alpha += 0.01
-            if event.key == pyg.K_RIGHT:
-                mops.alpha -= 0.01
-            if event.key == pyg.K_COMMA:
-                mops.sigma -= 0.01
-            if event.key == pyg.K_PERIOD:
-                mops.sigma += 0.01
+    # run the workspace
+    ws.run(center_point, events)
+    
+    x, y = ws.get_point_at_XYcoord([-(50 * cos(ang)), -(50 * sin(ang))])
+    ws2.run([x, y, 50], events)
+    
+    x, y = ws2.get_point_at_XYcoord([0, 0])
+    ws3.run([x, y, 50], events)
+    
+    x, y = ws3.get_point_at_XYcoord([-(20 * sin(ang)), -(20 * sin(ang))])
+    ws4.run([x, y, 50], events)
+    
+    ang += 0.01
+    
+    pyg.draw.line(ws.scrn, (234, 24, 242), (ws.get_point_at_XYcoord([0, 0])), (ws2.get_point_at_XYcoord([0, 0])), width=5)
+    pyg.draw.line(ws.scrn, (30, 223, 34), (ws2.get_point_at_XYcoord([0, 0])), (ws3.get_point_at_XYcoord([0, 0])), width=5)
+    pyg.draw.line(ws.scrn, (23, 43, 123), (ws3.get_point_at_XYcoord([0, 0])), (ws4.get_point_at_XYcoord([0, 0])), width=5)
+    
+# def handle_join_grid(j0_ws, center_point, events):
+#     j0_ws.run(center_point, events)
+    # pass
 
 if __name__ == "__main__":
     # initialize pygame
@@ -84,14 +78,19 @@ if __name__ == "__main__":
     # initialize menus
     tool_menu = Menu(screen, ORANGE, grid_columns=20, grid_rows=40,
                     title_col_span=[2, 17], title_row_span=[1, 3], menu_title="Tools")
-    # grid nesting example
-    # tools_innermenu = Menu(screen, (255, 255, 255), 10, 8, menu_title="", title_row_span=[1, 3], title_col_span=[1, 3])
     settings_menu = Menu(screen, MAX_RED, grid_columns=20, grid_rows=40,
                     title_col_span=[2, 17], title_row_span=[1, 3], menu_title="Settings")
     comms_menu = Menu(screen, PRUSSIAN_BLUE, grid_columns=20, grid_rows=40,
                     title_col_span=[2, 17], title_row_span=[1, 3], menu_title="Comms")
     joint_viz_menu = Menu(screen, MAX_YELLOW_RED, grid_columns=20, grid_rows=40,
                     title_col_span=[2, 17], title_row_span=[1, 3], menu_title="Joint Viz")
+    
+    # create workspace
+    workspace = Coordinate_grid(screen, pixel_axis_length=400, perceived_axis_length=200, grid_rows=10, grid_columns=10)
+    
+    joint_ws = Coordinate_grid(screen, pixel_axis_length=80, perceived_axis_length=40, grid_rows=4, grid_columns=4)
+    joint_ws2 = Coordinate_grid(screen, pixel_axis_length=80, perceived_axis_length=40, grid_rows=4, grid_columns=4)
+    joint_ws3 = Coordinate_grid(screen, pixel_axis_length=80, perceived_axis_length=40, grid_rows=4, grid_columns=4)
 
     while True:
         clock.tick(FPS)
@@ -115,7 +114,7 @@ if __name__ == "__main__":
         # handle menus - Menus get draw in the order they are run here
         master_menu.run([0, 0, winfo.current_w, winfo.current_h])
         
-        handle_model_viz(winfo.current_w, winfo.current_h, screen, events)
+        handle_workspace(workspace, [winfo.current_w//2, winfo.current_h//2, 1], events, joint_ws, joint_ws2, joint_ws3)
         
         tool_menu.run(
             master_menu.menu_grid.get_span(col_span=[0, 6], row_span=[0, 39])
